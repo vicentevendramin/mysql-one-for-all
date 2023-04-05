77f786fb2567daac7584165054fa5d3d871818ff
Vicente Vendramin <guesser.vicente@gmail.com>
2023-04-05 16:44:39 -0300

    Desafio 13 - Exibe a relação de pessoas usuárias e favoritadas por faixa etária

---
+++ challenges/desafio13.sql
@@ -0,0 +1,11 @@
+SELECT CASE
+	WHEN u.user_age <= 30 THEN 'Até 30 anos'
+  WHEN u.user_age > 30 AND u.user_age <= 60 THEN 'Entre 31 e 60 anos'
+  WHEN u.user_age > 60 THEN 'Maior de 60 anos'
+END AS faixa_etaria,
+	COUNT(DISTINCT u.user_id) AS total_pessoas_usuarias,
+  COUNT(f.user_id) AS total_favoritadas
+FROM SpotifyClone.users u
+	LEFT JOIN SpotifyClone.favorite f ON u.user_id = f.user_id
+GROUP BY faixa_etaria
+ORDER BY faixa_etaria;
